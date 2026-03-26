ce2131e5aa7b1a032d2646b7a5ebfe031e72221a
williammartinsmario <wmartins-mario@hotmail.com>
2026-03-26 13:15:11 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+// Service Worker neutro para desativar cache antigo do GitHub Pages
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(key => caches.delete(key)));
+    await self.clients.claim();
+  })());
+});
+
+self.addEventListener('fetch', event => {
+  // Não intercepta nada. Deixa o navegador buscar a versão mais nova.
+});
